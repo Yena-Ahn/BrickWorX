d9d0e4926e78e4b1d8ec43b2fad62bfa5d06b18f
dampbacon <7982855+dampbacon@users.noreply.github.com>
2023-04-03 14:11:13 +1200

    move temp data

--- react-test-project/src/components/rubric/rubricContainer.js
+++ react-test-project/src/components/rubric/rubricContainer.js
@@ -1,9 +1,11 @@
 //stub
 import RubricRowV2 from "../rubric/rubricRowV2";
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 
-const RubricContainer = ({DATA}) => {
+const RubricContainer = () => {
+  const DATA = [{question_id:1, question_title:'how to abc', rubric_fields : [{text: "blahblah", grade: 3 },{text: "blahblah", grade: 3 },{text: "blahblah", grade: 4 }]},
+  {question_id:2, question_title:'how to eat an apple', rubric_fields : [{text: "hfmdhsmdhfhmfds", grade: 30 }]}]
   
   console.log('data')
 
@@ -11,6 +13,10 @@ const RubricContainer = ({DATA}) => {
   const [formFields, setFormFields] = useState([
     ...DATA
   ])
+
+  const onChangeSomeState = (newSomeState) => {
+    setFormFields(newSomeState);
+  };
 
   // const addFields = (index) => {
   //   let object = {
@@ -34,7 +40,7 @@ const RubricContainer = ({DATA}) => {
         {formFields.map((a,b)=>{
          return <div key={b} className='box2'>
           {/* {JSON.stringify(a)} */}
-         <RubricRowV2 question={a.rubric_fields} question_index={b} update_method={setFormFields} rubric_data={DATA}/>
+         <RubricRowV2 question={a.rubric_fields} question_index={b} update_method={onChangeSomeState} rubric_data={DATA}/>
          
          {JSON.stringify(a.rubric_fields)}
          {JSON.stringify(b)}</div>
