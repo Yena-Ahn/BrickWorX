3718162d026b752bc838f5ecf195a00209cf93eb
Francois Beetge <7982855+dampbacon@users.noreply.github.com>
2023-04-24 00:36:13 +1200

    incomplete marking thing

--- react-test-project/src/components/pages/Mark.js
+++ react-test-project/src/components/pages/Mark.js
@@ -1,9 +1,12 @@
+import MarkingComp from "../rubric/markingrubric";
+
 const Mark = () => {
   return (
     <div>
       <h1>
         Marking
       </h1>
+      <MarkingComp/>
     </div>
   );
 };
