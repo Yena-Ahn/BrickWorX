e5f85f24483697cd567dc369f0f3c2bcd9100915
OfficeMinus <93555690+James-Macaskill@users.noreply.github.com>
2023-05-04 19:03:59 +1200

    Uncommented

--- express_project/dbService.js
+++ express_project/dbService.js
@@ -1,24 +1,24 @@
-// const mysql = require("mysql");
-// const dotenv = require("dotenv");
-// dotenv.config();
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config();
 
-// config = {
-//     user: process.env.USER,
-//     host: process.env.HOST,
-//     port: process.env.DB_PORT,
-//     password: process.env.PASSWORD,
-//     database: process.env.DATABASE,
-//     connectionLimit: 10,
-//     multipleStatements: true
-// }
-
-
-// const db = mysql.createConnection(config);
-// db.connect((err) => {
-//     if (err) throw err;
-//     console.log("Connected to Database.")
-// })
+config = {
+    user: process.env.USER,
+    host: process.env.HOST,
+    port: process.env.DB_PORT,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    connectionLimit: 10,
+    multipleStatements: true
+}
 
 
+const db = mysql.createConnection(config);
+db.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to Database.")
+})
 
-// module.exports = {connection: db};
+
+
+module.exports = {connection: db};
