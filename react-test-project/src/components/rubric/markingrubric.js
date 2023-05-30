184aed572638a6364aee51bdca70cb762450d7f9
ItsAmeKen <kishoratono14@gmail.com>
2023-05-31 00:44:04 +1200

    add className for styling

--- react-test-project/src/components/rubric/markingrubric.js
+++ react-test-project/src/components/rubric/markingrubric.js
@@ -463,8 +463,8 @@ const MarkingComp = ({setdefualtassignment}) => {
 					</div>
 					{/* issues deal with later */}
 					<div className="row-section__inner shadow" key={(index+1)*69}>
-						<textarea value={feedback[index]||''}  key={(index+1)*420} onChange={(e) => handleFeedbackData(question.id, e)}/>
-						<Button className='btn btn-warning' size="lg" onClick={() => setDefaultComments(index)}>Set feedback as default</Button>
+						<textarea className="feedback_box" value={feedback[index]||''}  key={(index+1)*420} onChange={(e) => handleFeedbackData(question.id, e)}/>
+						<Button className='btn btn-warning feedback_default' size="lg" onClick={() => setDefaultComments(index)}>Set feedback as default</Button>
 					</div>
 					</div>
 					
