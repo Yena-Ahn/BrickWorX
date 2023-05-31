4d0323d2f85f408eac524591bf01bcc001c8cc6d
ItsAmeKen <kishoratono14@gmail.com>
2023-06-01 02:47:04 +1200

    tweak and organising

--- react-test-project/src/components/upload/uploadmodal.js
+++ react-test-project/src/components/upload/uploadmodal.js
@@ -1,22 +1,22 @@
-
 import React from 'react';
 import { Button, Modal} from "react-bootstrap";
 
-function uploadModal(){
-    
-    return(
-    <Modal
-    show={this.props.isOpen}
-    onHide={this.props.closeModal}>
-
-<Modal.Header><strong>Upload Successful!</strong></Modal.Header>
-                        <Modal.Body>You're now ready to create a rubric!</Modal.Body>
-                        <Modal.Footer>
-                            <Button variant="secondary">Close</Button>
-                            <Button variant="primary">Create Rubric</Button>
-                        </Modal.Footer>
-</Modal>
-    )
+class uploadModal extends React.Component{
+    render() {
+        return (
+        <Modal show={this.props.isOpen} onHide={this.props.closeModal} style={{height:"90%"}}>
+            
+            <Modal.Header closeButton> <strong>Upload Successful!</strong> </Modal.Header>
+            
+            <Modal.Body> You're now ready to create a rubric! </Modal.Body>
+            
+            <Modal.Footer>
+                {/*<Button variant="secondary">Close</Button>*/}
+                <Button variant="primary" style={{width:"100%", padding:"10px"}} >Create Rubric</Button>
+            </Modal.Footer>
+        </Modal>
+        )
+    }
 }
    
 
