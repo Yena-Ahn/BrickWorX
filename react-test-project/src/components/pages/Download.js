86f026b51dfdeb1c0843a93b5d0b8c7cd10f9c49
dampbacon <7982855+dampbacon@users.noreply.github.com>
2023-06-05 19:09:35 +1200

    FIX

--- react-test-project/src/components/pages/Download.js
+++ react-test-project/src/components/pages/Download.js
@@ -8,7 +8,7 @@ const Download = () => {
       {/* <button className="btn btn-primary btn-lg" style={{width:500}} formAction="http://localhost:3001/s3DOWNLOAD?fn=CanvasExportExample.csv" >download</button> */}
       <div className="container shadow" style={{width:"650px"}}>
       <p>Your gradebook is now ready to download and upload to Canvas!</p>
-      <a href="http://localhost:3001/s3DOWNLOAD?fn=feedback.csv" className="btn btn-primary btn-lg downloadBtn" style={{ align:"center"}}> <Icon.Download/> Download Gradebook</a>
+      <a href="http://localhost:3001/s3DOWNLOAD?fn=CanvasExportExample.csv" className="btn btn-primary btn-lg downloadBtn" style={{ align:"center"}}> <Icon.Download/> Download Gradebook</a>
       <p style={{marginTop:"5px"}}>Thank you for using BrickWorX!</p>
       </div>
     </div>
